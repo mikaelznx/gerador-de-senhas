d2d731d56a7c50df7d9eb8b9f784c0ad483ab880
mikael pereira <99798475+mikaelznx@users.noreply.github.com>
2023-02-25 15:54:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+    var length = 16;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    document.getElementById("password").innerHTML = password;
+  }
+  
